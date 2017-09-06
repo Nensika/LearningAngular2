e3a87a07ad62ad7835451dfd7eea402ff518b397
narandjelovic <nevena.arandjelovic@telenor.rs>
2017-09-06 15:52:14 +0200

    Pozivanje komponente todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { TodosComponent} from './todos/todos.component';
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1>` ,
+  template: `<h1>{{title}}</h1>
+  <app-todos></app-todos>` ,
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'app works!';
